223ecb3f9477c04b07200239930b99ee67c61734
Titis Novianti <titisnvnt@gmail.com>
2025-12-09 22:41:11 +0700

    style: tambahkan komentar untuk method main

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("Hello World");
+    }
+}
+=======
+        System.out.println("Hello Java");
+    }
+}
+>>>>>>> ff6fcf63d7e7f7875fcf550953e4f9f525920f1a
