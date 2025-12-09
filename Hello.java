64c8d607d34240f7e9ecef91fd4e0a0705509ab9
Titis Novianti <titisnvnt@gmail.com>
2025-12-09 23:27:18 +0700

    style: menambahkan comment pada println

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     //add comment
     public static void main(String[] args) {
+        //menampilkan text
         System.out.println("Hello ini adalah latihan-git");
     }
 }
