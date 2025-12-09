6721e80cfda43363ae7bd6ee0ede79e91ea282fe
Titis Novianti <titisnvnt@gmail.com>
2025-12-09 23:10:35 +0700

    first commit

--- Hello.java
+++ Hello.java
@@ -1,17 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
         System.out.println("Hello World");
     }
 }
-=======
-        System.out.println("Hello Java");
-    }
-}
->>>>>>> ff6fcf63d7e7f7875fcf550953e4f9f525920f1a
-=======
-        System.out.println("Hello Java");
-    }
-}
->>>>>>> ff6fcf63d7e7f7875fcf550953e4f9f525920f1a
+
